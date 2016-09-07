b55f435f7dc1351c7a2bc0c9efb4995941336f06
klbm9999 <klbm9999@gmail.com>
2016-09-08 03:48:39 +0530

    Different functions for query and connection made.. just one error and we are good to go for the next step, ie start generation of the html files

--- mysqlconn.java
+++ mysqlconn.java
@@ -3,6 +3,16 @@ import java.sql.*;
 class mysqlconn {
 	public static void main(String[] args) {
 
+		Connection c;
+		mysql_connect();	
+		mysql_query(c);
+		mysql_connection_close(c);
+
+		System.out.println("Successful Connection");
+  
+	}
+
+	public static Connection mysql_connect() throws Exception {
 		String dbhost = "jdbc:mysql://localhost:3306/Test";
 		String dbuser = "root";
 		String dbpass = "";
@@ -10,21 +20,26 @@ class mysqlconn {
 		System.out.println("start");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection(dbhost,dbuser,dbpass);
-
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("select Customer_id, Customer_name from Customers");
-
-			while(rs.next())  
-				System.out.println(rs.getInt(1)+" : C_ID\n"+rs.getString(2)+" : C_Name");  
-
-			con.close();
+			con = DriverManager.getConnection(dbhost,dbuser,dbpass);
 		}
 		catch (Exception c) {
-			System.out.println("caught mysql exception");
+			System.out.println("Could not connect to the Database");
 			c.printStackTrace();
 		}
-		System.out.println("Successful Connection");
-  
+
+		return con;
+	}
+
+	public static void mysql_query(Connection con) throws Exception {
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("select Customer_id, Customer_name from Customers");
+
+		while(rs.next())  
+			System.out.println(rs.getInt(1)+" : C_ID\n"+rs.getString(2)+" : C_Name");  
+
+	}
+
+	public static void mysql_connection_close(Connection con) throws Exception {
+		con.close();
 	}
 }
