3c08913af71f723bbd58a31a3b5ea9d16833ff37
klbm9999 <klbm9999@gmail.com>
2016-09-09 03:26:57 +0530

    generated the required html file proto-prototype is successful

--- mysqlconn.java
+++ mysqlconn.java
@@ -1,5 +1,5 @@
 import java.sql.*;
-
+import java.io.*;
 
 class mysqlconn {
 
@@ -20,10 +20,30 @@ class mysqlconn {
 
 	public static void mysql_query(Connection con) throws Exception {
 		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery("select Customer_id, Customer_name from Customers");
+		ResultSet rs = st.executeQuery("select Customer_id, Customer_name from Customers"); 
+		html_gen(rs);
+	}
 
-		while(rs.next())  
-			System.out.println(rs.getInt(1)+" : C_ID\n"+rs.getString(2)+" : C_Name");  
+	public static void html_gen(ResultSet rs) {
+		
+		/*FileOutputStream out = null;
+		out = new FileOutputStream("op.html");*/
+		try {
+			PrintWriter out = new PrintWriter("op.html");
+			StringBuilder start = new StringBuilder("<html><body>");
+			StringBuilder end = new StringBuilder("</body></html>");
+			StringBuilder content = new StringBuilder();
+			while(rs.next())  
+			 	content.append(rs.getInt(1)).append(" ").append(rs.getString(2)).append("<br>"); 
+
+			start = start.append(content).append(end); 
+			out.println(start.toString());
+			out.close();
+		}
+		catch (Exception e) {
+			System.out.println("Couldnt generate html");
+			e.printStackTrace();
+		}
 
 	}
 
