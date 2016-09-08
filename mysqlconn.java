b0c91912ae1db358fdc4dd78eb221394209bba9a
klbm9999 <klbm9999@gmail.com>
2016-09-08 18:04:33 +0530

    completed seperation of db connection, query and connection closing and no errors

--- mysqlconn.java
+++ mysqlconn.java
@@ -1,33 +1,21 @@
 import java.sql.*;
 
+
 class mysqlconn {
+
 	public static void main(String[] args) {
 
-		Connection c;
-		mysql_connect();	
-		mysql_query(c);
-		mysql_connection_close(c);
+		try {
+			Connection con = connect_db.getConnection();
+			mysql_query(con);
+			mysql_connection_close(con);
+		}
+		catch (Exception e) {
+			System.out.println("Couldnt query");
+		}
 
 		System.out.println("Successful Connection");
   
-	}
-
-	public static Connection mysql_connect() throws Exception {
-		String dbhost = "jdbc:mysql://localhost:3306/Test";
-		String dbuser = "root";
-		String dbpass = "";
-
-		System.out.println("start");
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection(dbhost,dbuser,dbpass);
-		}
-		catch (Exception c) {
-			System.out.println("Could not connect to the Database");
-			c.printStackTrace();
-		}
-
-		return con;
 	}
 
 	public static void mysql_query(Connection con) throws Exception {
@@ -41,5 +29,33 @@ class mysqlconn {
 
 	public static void mysql_connection_close(Connection con) throws Exception {
 		con.close();
+	}
+}
+
+class connect_db {
+
+	static Connection con = null;
+	static String dbhost = "jdbc:mysql://localhost:3306/Test";
+	static String dbuser = "root";
+	static String dbpass = "";
+
+
+	public static Connection getConnection() {
+		if (con != null) return con;
+		return getConnection(dbhost,dbuser,dbpass);
+	}
+
+	public static Connection getConnection(String dbhost, String dbuser, String dbpass) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(dbhost,dbuser,dbpass);
+			return con;
+		}
+		catch (Exception c) {
+			System.out.println("Could not connect to the Database");
+			c.printStackTrace();
+		}
+
+		return con;
 	}
 }
